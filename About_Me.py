de0c69a925f5091d5f89456126d07e81cbbf1570
mrizwanali <mrizwanali@gmail.com>
2020-02-07 00:09:17 +0500

    Created the First Python File in Jupyter

---
+++ About_Me.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+topic = "<---- About Myself ---->"
+
+
+# In[2]:
+
+
+name = "Muhammad Rizwan Ali"
+
+
+# In[3]:
+
+
+edu = "BS (Electronics)"
+
+
+# In[4]:
+
+
+uni = "SSUET"
+
+
+# In[5]:
+
+
+bat = "First Batch - 1994"
+
+
+# In[6]:
+
+
+prof = "Project Manager"
+
+
+# In[7]:
+
+
+place = "Karachi"
+
+
+# In[8]:
+
+
+com = "Self Employed"
+
+
+# In[9]:
+
+
+spec = "Linux and Middleware"
+
+
+# In[10]:
+
+
+exp = "16 years"
+
+
+# In[11]:
+
+
+f_word = "<------ That's All ----->"
+
+
+# In[12]:
+
+
+print("\n")
+
+
+# In[13]:
+
+
+print(topic, "\n")
+
+
+# In[14]:
+
+
+print("Name: ", name, "\n")
+
+
+# In[15]:
+
+
+print("Education: ", edu, "\n")
+
+
+# In[16]:
+
+
+print("University: ", uni, "\n")
+
+
+# In[17]:
+
+
+print("Batch: ", bat, "\n")
+
+
+# In[18]:
+
+
+print("Profession: ", prof, "\n")
+
+
+# In[19]:
+
+
+print("Place: ", place, "\n")
+
+
+# In[20]:
+
+
+print("Company: ", com, "\n")
+
+
+# In[21]:
+
+
+print("Specialization: ", spec, "\n")
+
+
+# In[22]:
+
+
+print("Experience: ", exp, "\n")
+
+
+# In[23]:
+
+
+print(f_word, "\n")
+
